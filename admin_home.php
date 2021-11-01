4f0e7d02ff432342d5e0ee1ef3955df9d6ef4529
omkarnangare <87194000+omkarnangare@users.noreply.github.com>
2021-11-02 00:33:55 +0530

    Add files via upload

---
+++ admin_home.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>STUDYTEMPLE.com</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		
+		
+		<meta name="author" content="Codrops" />
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+
+<script type="text/javascript" src="js/jquery-1.4.2.min.js" ></script>
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-replace.js"></script>
+<script type="text/javascript" src="js/Myriad_Pro_300.font.js"></script>
+<script type="text/javascript" src="js/Myriad_Pro_400.font.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+		
+<style>
+.noticepanel {
+    background-color:#b9b9b9;
+    padding:50px;
+}
+
+.button {
+  display: inline-block;
+  padding: 10px 15px;
+  font-size: 15px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  color: #fff;
+  background-color: #4CAF50;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 0 9px #999;
+}
+
+.button:hover {background-color: #3e8e41}
+
+.button:active {
+  background-color: #3e8e41;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
+</style>
+</head>
+<body id="page1">
+<!-- START PAGE SOURCE -->
+<div class="wrap">
+  <header>
+    <div class="container">
+      <h1><a href="admin_home.php">STUDYTEMPLE.com</a></h1>
+      <nav>
+       <ul>
+          
+          <li class="current" ><a href="admin_home.php" class="m4">admin</a></li>
+        </ul>
+      </nav>
+      <form action="#" id="search-form">
+        <fieldset>
+          <div class="rowElem">
+            <input type="text">
+            <a href="#">Search</a></div>
+        </fieldset>
+      </form>
+    </div>
+  </header>
+  <div class="container">
+ 
+<br><br><br>
+	 
+  <center> 
+<img src='images/admin1.png'/>
+<br><br><br><br>
+  <table class='inside' border=1>
+
+<tr>
+	<td align='center'><button class='panelbtn' onclick="window.location.href='activate_students.php'">Activate Students</button></td>
+	<td align='center'><button class='panelbtn' onclick="window.location.href='activate_teacher.php'">Activate Teacher</button></td>
+	<td align='center'><button class='panelbtn' onclick="window.location.href='admin_inbox.php'">Inbox</button></td>
+</tr>
+<tr>
+	<td align='center'><button class='panelbtn' onclick="window.location.href='assignments.php'">Manage Assignments</button></td>
+	<td align='center'><button class='panelbtn' onclick="window.location.href='quiz/questions.php'">Manage Quiz</button></td>
+	<td align='center'><button class='panelbtn' onclick="window.location.href='notice.php'">Manage Notice</button></td>
+</tr>
+</table>  
+<br><br><br><br><br>
+<button class='button' onclick="window.location.href='logout.php'">Logout</button>
+</center>
+
+</div>
+<br><br><br><br><br>
+<footer>
+  <div class="footerlink">
+                    <p class="lf">Copyright &copy; 2020 <a href="#">StudyTemple.com</a> - All Rights Reserved</p>
+    
+    <div style="clear:both;"></div>
+  </div>
+</footer>
+<script type="text/javascript"> Cufon.now(); </script>
+
+<!-- END PAGE SOURCE -->
+</body>
+</html>
